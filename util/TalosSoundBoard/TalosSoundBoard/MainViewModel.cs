cc4619606fbd0a75ba2d566e2e9fdc015774ae58
Josh Weeks <weeks.josh@gmail.com>
2023-02-27 14:45:31 -0500

    rig client

---
+++ util/TalosSoundBoard/TalosSoundBoard/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ReactiveUI;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Reactive;
+using System.Reactive.Threading.Tasks;
+using System.Windows.Input;
+using System.Reactive.Linq;
+
+namespace TalosSoundBoard;
+
+internal class MainViewModel : ReactiveObject
+{
+    public MainViewModel()
+    {
+        
+        _clips = this.GetClips().ToObservable().ToProperty(this, nameof(Clips), scheduler: RxApp.MainThreadScheduler);
+        this.PlayCommand = ReactiveCommand.CreateFromTask<string>(this.Play);
+    }
+
+    private async Task<Unit> Play(string fileName)
+    {
+        var client = new HttpClient();
+        await client.GetAsync($"http://gordon:5851/play?fileName={fileName}");
+        return Unit.Default;
+    }
+
+    public ReactiveCommand<string, Unit> PlayCommand { get; }
+    public ICommand TestCommand { get; }
+
+    public IEnumerable<Clip> Clips => _clips.Value;
+    ObservableAsPropertyHelper<IEnumerable<Clip>> _clips;
+
+    private async Task<IEnumerable<Clip>> GetClips()
+    {
+        var client = new HttpClient();
+        return await client.GetFromJsonAsync<IEnumerable<Clip>>("http://gordon:5851/list");        
+    }
+
+
+}
+
+record class Clip(string Title, string FileName);
--- util/TalosSoundBoard/TalosSoundBoardService/Program.cs
+++ util/TalosSoundBoard/TalosSoundBoardService/Program.cs
@@ -3,8 +3,6 @@ using System.Media;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,12 +10,11 @@ builder.Services.Configure<SoundBoardSettings>(builder.Configuration.GetSection(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 var playerMap = app.Services.GetRequiredService<IOptions<SoundBoardSettings>>().Value.Clips!
     .ToDictionary(
@@ -44,11 +41,6 @@ app.MapGet("/Play", (string fileName) =>
 });
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 internal class Clip
 {
